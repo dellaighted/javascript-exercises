fc57b172f7572fb74aabf7cef7a83993ebfea4f3
Della Maret <dellamaret01@gmail.com>
2024-06-17 20:39:22 -0400

    my solution to problems 1-4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let result = "";
+    if(num < 0){
+        result = "ERROR";
+    }
+    if(num == 0){
+        result = '';
+    }
+    while(num > 0){
+        result += str;
+        num--;
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let result = "";
+    let strlength = str.length;
+    if (strlength < 0){
+        result = "ERROR";
+    }
+    while (strlength >= 0){
+        result += str.charAt(strlength-1);
+        strlength--;
+    }
+    return result;
 };
 
 // Do not edit below this line
