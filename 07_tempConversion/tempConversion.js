01d673cc6a5f347a5b11cba092b31d7510547313
Della Maret <dellamaret01@gmail.com>
2024-06-24 11:28:45 -0400

    finished problems 6 and 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  result = (temp - 32) * 5/9;
+  if(result % 1 == 0){
+    return result;
+  }else{
+    return Number(result.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  result = (temp * 9/5) + 32;
+  if(result % 1 == 0){
+    return result;
+  }else{
+    return Number(result.toFixed(1));
+  }
 };
 
 // Do not edit below this line
