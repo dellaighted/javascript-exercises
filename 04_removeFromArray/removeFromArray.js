7daf42b2b19e46555d6cb6430c0b86b3388eba00
Della Maret <dellamaret01@gmail.com>
2024-06-24 10:31:44 -0400

    finished problem 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    i = 0;
+    while(i < arr.length){
+        for(arg of items){
+            index = arr.indexOf(arg);
+            if(index > -1){
+                arr.splice(index, 1);
+            }else{
+                i++;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
