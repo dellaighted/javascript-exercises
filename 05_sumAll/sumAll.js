c54e6429d5036068b2ee41111c1ccd665ebff09e
Della Maret <dellamaret01@gmail.com>
2024-06-24 10:44:33 -0400

    finished problem 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(i, j) {
+    result = 0;
 
+    if(typeof(i) != "number" || typeof(j) != "number" ||
+        i < 0 || j < 0){
+        return 'ERROR';
+    }
+
+    start = 0;
+    end = 0;
+
+    if(i < j){
+        start = i;
+        end = j;
+    }else{
+        start = j;
+        end = i;
+    }
+
+    while(start <= end){
+        result += start;
+        start++;
+    }
+    return result;
 };
 
 // Do not edit below this line
