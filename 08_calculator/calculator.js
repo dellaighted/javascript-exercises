59fd310efd10af9cfa2be3371aed480147aadca0
Della Maret <dellamaret01@gmail.com>
2024-08-20 14:40:11 -0400

    finished exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const result = arr.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const result = arr.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
+  return result
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y)
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let sum = 1;
+  while(x >= 0){
+    if (x == 0){
+      sum = sum * 1
+    }else{
+      sum = sum * x;
+    }
+    x = x-1;
+  }
+  return sum;
 };
 
 // Do not edit below this line
